38e7e901ee6a72ebd8024d99657bca20ab392126
Marcos Vinicius <marcosvinicius.udia1256@gmail.com>
2022-03-24 14:44:38 -0300

    colocando sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import estilo from '../../styles/Home.module.scss';
+
 export default function Home() {
   return (
     <div>
-      <h1>Olá Dev!</h1>
+      <h1 className={estilo.title}>
+        Olá <span>Dev</span>!
+      </h1>
     </div>
   );
 }
