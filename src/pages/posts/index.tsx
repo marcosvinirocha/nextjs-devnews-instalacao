ea00cf36aadd1598eba5424185269ee51d841fbc
Marcos Vinicius <marcosvinicius.udia1256@gmail.com>
2022-04-14 17:57:04 -0300

    config api prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,15 @@
 import { GetStaticProps } from 'next';
 import SEO from '../../components/Header/SEO';
-
+import Link from 'next/link';
+import styles from './posts.module.scss';
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 interface Post {
-  id: string;
+  slug: string;
   title: string;
+  excerpt: string;
+  update: string;
 }
 
 interface PostsProps {
@@ -11,25 +17,52 @@ interface PostsProps {
 }
 export default function Posts({ posts }: PostsProps) {
   return (
-    <div>
+    <>
       <SEO title="Posts " />
-      <h1>Listagem de Posts</h1>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    </div>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <Link key={post.slug} href="#">
+              <a href="#">
+                <time>{post.update} </time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
 
-export const getStaticProps: GetStaticProps<PostsProps> = async () => {
-  const res = await fetch('http://localhost:3333/posts');
-  const posts = await res.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      orderings: '[document.first_publication_date desc]',
+      fetch: ['post.title', 'post.content'],
+    },
+  );
+  const posts = response.results.map(post => ({
+    slug: post.uid,
+    title: post.data.title,
+    content:
+      post.data.content.find(content => content.type === 'paragraph')?.text ??
+      '',
+    update: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    }),
+  }));
+  console.log(response.results);
+
   return {
     props: {
       posts,
     },
-    revalidate: 5, //in seconds reload component e search new data in API
+    revalidate: 60 * 60 * 12, //in seconds reload component e search new data in API
   };
 };
