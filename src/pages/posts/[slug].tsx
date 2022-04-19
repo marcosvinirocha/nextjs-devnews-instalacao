3b0238ad5478538b0c3aa1545f9b1c9e9c2d5874
Marcos Vinicius <marcosvinicius.udia1256@gmail.com>
2022-04-19 16:00:51 -0300

    update page post

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,70 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import SEO from '../../components/Header/SEO';
+import styles from './post.module.scss';
+import Prismic from '@prismicio/client';
+import { getPrismicClient } from '../../services/prismic';
+
+interface PostProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    update: string;
+  };
+}
+
+export default function Posts({ post }: PostProps) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <>
+      <SEO title="Post " />
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.update}</time>
+          <div className={styles.content}>{post.content} </div>
+        </article>
+      </main>
+    </>
+  );
+}
+
+//criar pagina estatica com parametros
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async context => {
+  const { slug } = context.params;
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('post', String(slug), {});
+  const post = {
+    slug,
+    title: response.data.title,
+    content: response.data.title,
+    update: new Date(response.last_publication_date).toLocaleDateString(
+      'pt-BR',
+      {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      },
+    ),
+  };
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60 * 60 * 12, // In seconds
+  };
+};
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -22,8 +22,8 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <Link key={post.slug} href="#">
-              <a href="#">
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
                 <time>{post.update} </time>
                 <strong>{post.title}</strong>
                 <p>{post.excerpt}</p>
